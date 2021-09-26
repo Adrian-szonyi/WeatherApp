0847589f44cb4f28b62235e5773ac37c9ace4d53
unknown <aszonyi49@gmail.com>
2021-09-26 22:47:49 +1000

    Current weather and 5 day forecast populating

--- script.js
+++ script.js
@@ -1,9 +1,87 @@
-function displayTime() {
-    var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
-    timeDisplayEl.text(rightNow);
-  }
-  displayTime();
-  var mykey = config.MY_KEY;
 
-  // https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-  // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+var mykey = My_KEY;
+var timeDisplayEl =  document.querySelector("#time-display");
+var cardTitle = document.querySelector(".card-title")
+var cityname = document.querySelector("#cityname");
+var humidity = document.querySelector("#humidity");
+var temperature = document.querySelector("#temp");
+var UVindex = document.querySelector("#UV");
+var windspeed = document.querySelector("#windspeed");
+var button = document.querySelector("#generateweather");
+var Day1Temp = document.querySelector("#Day1Temp");
+var Day1Windspeed = document.querySelector("#Day1Windspeed");
+var Day1Humidity = document.querySelector("#Day1Humidity");
+var Day1UV = document.querySelector("#Day1UV");
+var Day1Icon = document.querySelector("#Day1Icon");
+var Day2Temp = document.querySelector("#Day2Temp");
+var Day2Windspeed = document.querySelector("#Day2Windspeed");
+var Day2Humidity = document.querySelector("#Day2Humidity");
+var Day2UV = document.querySelector("#Day2UV");
+var Day2Icon = document.querySelector("#Day2Icon");
+var Day3Temp = document.querySelector("#Day3Temp");
+var Day3Windspeed = document.querySelector("#Day3Windspeed");
+var Day3Humidity = document.querySelector("#Day3Humidity");
+var Day3UV = document.querySelector("#Day3UV");
+var Day3Icon = document.querySelector("#Day3Icon");
+var Day4Temp = document.querySelector("#Day4Temp");
+var Day4Windspeed = document.querySelector("#Day4Windspeed");
+var Day4Humidity = document.querySelector("#Day4Humidity");
+var Day4UV = document.querySelector("#Day4UV");
+var Day4Icon = document.querySelector("#Day4Icon");
+var Day5Temp = document.querySelector("#Day5Temp");
+var Day5Windspeed = document.querySelector("#Day5Windspeed");
+var Day5Humidity = document.querySelector("#Day5Humidity");
+var Day5UV = document.querySelector("#Day5UV");
+var Day5Icon = document.querySelector("#Day5Icon");
+
+
+async function getWeatherData() {
+    var cityforecast = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityname.value+'&appid='+mykey+'')
+    .then(response => response.json())
+    console.log(cityforecast)
+    var oneCall = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityforecast.coord.lat}&lon=${cityforecast.coord.lon}&appid=${mykey}&units=metric`)
+    .then(response => response.json())
+    console.log(oneCall)
+
+    cardTitle.textContent= cityname.value;
+    temperature.textContent= "Current Temp: " + oneCall.current.temp + " C"
+    windspeed.textContent= "Wind Speed: " + cityforecast.wind.speed;
+    UVindex.textContent= "UV Index: " + oneCall.current.uvi;
+    humidity.textContent = "Humidity: " + oneCall.current.humidity;
+    Day1Temp.textContent = "Forecast Temp: " + oneCall.daily[1].temp.day;
+    Day1Humidity.textContent = "Humidity: " + oneCall.daily[1].humidity;
+    Day1UV.textContent = "UV Index: " + oneCall.daily[1].uvi;
+    Day1Windspeed.textContent = "Wind Speed: " + oneCall.daily[1].wind_speed;
+    Day2Temp.textContent = "Forecast Temp: " + oneCall.daily[2].temp.day;
+    Day2Humidity.textContent = "Humidity: " + oneCall.daily[2].humidity;
+    Day2UV.textContent = "UV Index: " + oneCall.daily[2].uvi;
+    Day2Windspeed.textContent = "Wind Speed: " + oneCall.daily[2].wind_speed;
+    Day3Temp.textContent = "Forecast Temp: " + oneCall.daily[3].temp.day;
+    Day3Humidity.textContent = "Humidity: " + oneCall.daily[3].humidity;
+    Day3UV.textContent = "UV Index: " + oneCall.daily[3].uvi;
+    Day3Windspeed.textContent = "Wind Speed: " + oneCall.daily[3].wind_speed;
+    Day4Temp.textContent = "Forecast Temp: " + oneCall.daily[4].temp.day;
+    Day4Humidity.textContent = "Humidity: " + oneCall.daily[4].humidity;
+    Day4UV.textContent = "UV Index: " + oneCall.daily[4].uvi;
+    Day4Windspeed.textContent = "Wind Speed: " + oneCall.daily[4].wind_speed;
+    Day5Temp.textContent = "Forecast Temp: " + oneCall.daily[5].temp.day;
+    Day5Humidity.textContent = "Humidity: " + oneCall.daily[5].humidity;
+    Day5UV.textContent = "UV Index: " + oneCall.daily[5].uvi;
+    Day5Windspeed.textContent = "Wind Speed: " + oneCall.daily[5].wind_speed;
+}
+
+cityname.addEventListener("keydown", function(event){
+    if (event.keyCode === 13){
+        getWeatherData();
+    }
+})
+button.addEventListener("click", function(){
+      getWeatherData();
+      // displayTime();
+  });
+  // function displayTime() {
+  //   var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+  //   var newcity = rightNow.tz("Sydney/Australia").format("HH:MM");
+  //   timeDisplayEl.text(newcity);
+  // }
+
